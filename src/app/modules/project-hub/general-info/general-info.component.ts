ce69dbbf84c21e406b5c5df976791201f91b59ee
Mannat Dev <mannat.dev@takeda.com>
2022-07-05 15:30:25 -0400

    MH ID3092 General Info Page

--- src/app/modules/project-hub/general-info/general-info.component.ts
+++ src/app/modules/project-hub/general-info/general-info.component.ts
@@ -1,6 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
 import { ProjectApiService } from '../common/project-api.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { GlobalFiltersDropDown } from 'app/shared/global-filters';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+
+
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+// interface ProjectType {
+//   value: string;
+//   viewValue: string;
+// }
 
 @Component({
   selector: 'app-general-info',
@@ -10,14 +21,32 @@ import { ProjectApiService } from '../common/project-api.service';
 export class GeneralInfoComponent implements OnInit {
   projectid: string = ""
   projectdata: any = {}
+  checked = false;
+  // projectType: ProjectType[] = [
+  //   {value: 'Standard Project / Program', viewValue: 'Standard Project / Program'},
+  //   {value: 'Simple Project', viewValue: 'Simple Project'}
+  // ];
+
+  //isOetoggle = false;
   constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute) { }
 
+
+
   ngOnInit(): void {
+
+
     this.projectid = this._Activatedroute.parent.snapshot.paramMap.get("id");
     this.apiService.getGeneralInfoData(this.projectid).then((res: any) => {
-      this.projectdata = res.projectData
-      console.log("General Info")
+      this.projectdata = res
+
+      // if(isOeproject == "true")
+      // this.projectdata.isOeproject = true
+      // else
+      // this.projectdata.isOeproject = false
+
       console.log(this.projectdata)
+
+
     })
   }
 
