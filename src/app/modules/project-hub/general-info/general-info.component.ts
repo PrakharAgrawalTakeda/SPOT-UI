64bcfee8479d3a2e685cb8375d2c599f92437cee
Mannat Dev <mannat.dev@takeda.com>
2022-07-11 14:57:26 -0400

    generalinfo

--- src/app/modules/project-hub/general-info/general-info.component.ts
+++ src/app/modules/project-hub/general-info/general-info.component.ts
@@ -6,11 +6,14 @@ import { GlobalFiltersDropDown } from 'app/shared/global-filters';
 import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-
+import * as moment from 'moment';
+import { HttpClient } from '@angular/common/http';
+import { GlobalVariables } from 'app/shared/global-variables';
 
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { map, Observable, startWith } from 'rxjs';
 import { MatChipInputEvent } from '@angular/material/chips';
+import { id } from '@swimlane/ngx-datatable';
 
 
 @Component({
@@ -21,6 +24,8 @@ import { MatChipInputEvent } from '@angular/material/chips';
 })
 export class GeneralInfoComponent implements OnInit {
 
+  // putUrl = GlobalVariables.apiurl + "PortoflioCenterData/{id}"
+emPortfolioOwnerId : string = ''
 selectedValue: any = {};
 
   myControl = new FormControl('');
@@ -31,13 +36,29 @@ selectedValue: any = {};
   emControl = new FormControl('');
   values = ''
   ems: any =  []
-  filteredEms: Observable<string[]>;
+  filteredEms: any;
 
  
   fruitCtrl = new FormControl('');
   filteredFruits: Observable<string[]>;
   otherimpactedproducts: string[] = [];
   @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+
+  generalinfoform = new FormGroup({
+   confidential: new FormControl(''),
+   title: new FormControl(''),
+  type: new FormControl(''),
+  tops: new FormControl(''),
+  createddate: new FormControl(''),
+  submittedby: new FormControl(''),
+  sponsor: new FormControl(''),
+   pm: new FormControl(''),
+   portfolioowner: new FormControl(''),
+    projectdesc: new FormControl(''),
+   OE: new FormControl(''),
+   techtransfer: new FormControl(''),
+   caps: new FormControl('')
+  })
 
   projectid: string = ""
   projectdata: any = {}
@@ -52,30 +73,30 @@ selectedValue: any = {};
     "products": [],
     "totalCAPEX": []
   }
-  filters: any = {
-    "portfolioOwner": [],
-    "phase": [],
-    "executionScope": [],
-    "people": [],
-    "products": [],
-    "state": [],
-    "totalCAPEX": [],
-    "gmsBudgetOwner": [],
-    "oeProjectType": [],
-    "projectType": [],
-    "fundingStatus": [],
-    "agileWorkstream": [],
-    "agileWave": [],
-    "primaryKPI": [],
-    "startegicYear": [],
-    "annualInitiatives": [],
-    "topsGroup": [],
-    "capsProject": [],
-    "projectName": []
-  }
+  // filters: any = {
+  //   "portfolioOwner": [],
+  //   "phase": [],
+  //   "executionScope": [],
+  //   "people": [],
+  //   "products": [],
+  //   "state": [],
+  //   "totalCAPEX": [],
+  //   "gmsBudgetOwner": [],
+  //   "oeProjectType": [],
+  //   "projectType": [],
+  //   "fundingStatus": [],
+  //   "agileWorkstream": [],
+  //   "agileWave": [],
+  //   "primaryKPI": [],
+  //   "startegicYear": [],
+  //   "annualInitiatives": [],
+  //   "topsGroup": [],
+  //   "capsProject": [],
+  //   "projectName": []
+  // }
 
 
-  constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute, private fb: FormBuilder) { 
+  constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute, private fb: FormBuilder, private http: HttpClient) { 
 
 
 
@@ -118,13 +139,28 @@ private _filterfruit(value: string): string[] {
 
     this.filteredEms = this.emControl.valueChanges.pipe(
       startWith(''),
-      map(values => this._filters(values || '')),
+      map(values => this.filterlist.portfolioOwner.filter(x=>x.portfolioOwner.includes(values) && x.isPortfolioOwner == true)),
     );
 
     this.projectid = this._Activatedroute.parent.snapshot.paramMap.get("id");
     this.apiService.getGeneralInfoData(this.projectid).then((res: any) => {
       this.projectdata = res
-
+      // this.generalinfoform.patchValue({
+      //   confidential: res.confidential,
+      //   title: res.title,
+      //   type: res.type,
+      //   tops: res.tops,
+      //   createddate: res.createddate,
+      //   submittedby: res.submittedby,
+      //   sponsor: res.sponsor,
+      //   pm: res.pm,
+      //   portfolioowner: res.portfolioowner,
+      //   projectdesc: res.projectdesc,
+      //   OE: res.OE,
+      //   techtransfer: res.techtransfer,
+      //   caps: res.caps
+      // })
+      this.emControl.patchValue(this.projectdata.enviornmentalPortfolio.portfolioOwner)
       console.log(this.projectdata)
       this.otherimpactedproducts = this.projectdata.otherImpactedProducts.map(x=>x.fullProductName)
       this.value = this.projectdata.primaryProduct.fullProductName
@@ -141,7 +177,7 @@ private _filterfruit(value: string): string[] {
       this.filterlist = data;
       this.options = this.filterlist.products.map(t=>t.fullProductName)
       this.filteredFruits = this.options
-      //console.log(this.options)
+      console.log(this.filterlist)
     })
     
   }
@@ -166,4 +202,31 @@ private _filterfruit(value: string): string[] {
        //console.log(this.filtersnew)
      }
   }
+
+  selectemPortfolio(option: any)
+  {
+    this.emPortfolioOwnerId = option;
+  }
+  // submitgeneralinfo() {
+  //   if (this.generalinfoform.valid) {
+  //     this.projectdata.projectData.problemTitle = this.generalinfoform.value.title
+  //     this.projectdata.projectData.isConfidential = this.generalinfoform.value.confidential
+  //     this.projectdata.projectData.problemType = this.generalinfoform.value.type
+  //     this.projectdata.pm.tops = this.generalinfoform.value.tops
+  //     this.projectdata.projectData.createdDate = moment(this.generalinfoform.value.createddate).format('YYYY-MM-DD[T]HH:mm:ss.sss[Z]')
+  //     this.projectdata.projectData.problemOwnerName = this.generalinfoform.value.submittedby
+  //     this.projectdata.sponsor.teamMemberName = this.generalinfoform.value.sponsor
+  //     this.projectdata.pm.pm = this.generalinfoform.value.pm
+  //     this.projectdata.pm.portfolioOwner = this.generalinfoform.value.portfolioowner
+  //     this.projectdata.projectData.projectDescription = this.generalinfoform.value.projectdesc
+  //     this.projectdata.projectData.isOeproject = this.generalinfoform.value.OE
+  //     this.projectdata.projectData.isTechTransfer = this.generalinfoform.value.techtransfer
+  //     this.projectdata.projectData.isCapsProject = this.generalinfoform.value.caps
+  //     // this.projectdata.enviornmentalPortfolio.portfolioOwner = this.emControl.value
+
+  //     // return this.http.put<any>(this.putUrl, id)
+  //     // // .subscribe(data => this.projectdata.projectData.problemUniqueId = data.id);
+
+  //   }
+  // }
 }
