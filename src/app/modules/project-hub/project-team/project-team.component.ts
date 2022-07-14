164d38e9ab2776c8d2c1b8597d3d45ffa04a6417
prakharagrawal <prakhar.agrawal@takeda.com>
2022-07-13 23:26:02 -0400

    MH 2883 start

--- src/app/modules/project-hub/project-team/project-team.component.ts
+++ src/app/modules/project-hub/project-team/project-team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MsalService } from '@azure/msal-angular';
 import { ProjectApiService } from '../common/project-api.service';
@@ -7,18 +7,20 @@ import { ProjectApiService } from '../common/project-api.service';
 @Component({
   selector: 'app-project-team',
   templateUrl: './project-team.component.html',
-  styleUrls: ['./project-team.component.scss']
+  styleUrls: ['./project-team.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProjectTeamComponent implements OnInit {
-  data: any = {}
+  teamMembers: any = []
   id: string = ''
+  isGrid: boolean = false
   constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.id = this._Activatedroute.parent.snapshot.paramMap.get("id");
     this.apiService.getmembersbyproject(this.id).then((res) => {
       console.log(res)
-      this.data.teamMembers = res
+      this.teamMembers = res
     })
   }
 
