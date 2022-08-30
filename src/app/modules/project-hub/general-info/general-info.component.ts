a60fda79fd8dcfdd0ffe802317c7d5229431b71a
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2022-08-30 17:11:35 -0400

    MH 2889 View

--- src/app/modules/project-hub/general-info/general-info.component.ts
+++ src/app/modules/project-hub/general-info/general-info.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms'
 import * as moment from 'moment';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { PortfolioApiService } from 'app/modules/portfolio-center/portfolio-api.service';
+import { AuthService } from 'app/core/auth/auth.service';
 
 @Component({
   selector: 'app-general-info',
@@ -16,6 +17,8 @@ import { PortfolioApiService } from 'app/modules/portfolio-center/portfolio-api.
   providers: [],
 })
 export class GeneralInfoComponent implements OnInit {
+  viewContent: boolean = false
+  lookUpData: any = []
   id: string = ""
   generalInfoData: any = {}
   filterCriteria: any = {}
@@ -35,12 +38,20 @@ export class GeneralInfoComponent implements OnInit {
     portfolioOwner: new FormControl({}),
     excecutionScope: new FormControl([]),
     enviornmentalPortfolio: new FormControl({}),
+    isOeproject: new FormControl(false),
+    oeprojectType: new FormControl({}),
+    isCapsProject: new FormControl(false),
+    isTechTransfer: new FormControl(false),
+    productionStepId: new FormControl(''),
+    campaignPhaseId: new FormControl(''),
+    campaignTypeId: new FormControl(''),
+    isQualityRef: new FormControl(false),
   })
   projectTypeDropDrownValues = ["Standard Project / Program", "Simple Project"]
   formFieldHelpers: any
-  constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute, private portApiService: PortfolioApiService) {
+  constructor(private apiService: ProjectApiService, private _Activatedroute: ActivatedRoute, private portApiService: PortfolioApiService, private authService: AuthService) {
 
-    this.generalInfoForm.controls.otherImpactedProducts.valueChanges.subscribe(res => {
+    this.generalInfoForm.controls.isOeproject.valueChanges.subscribe(res => {
       console.log(res)
     })
   }
@@ -51,24 +62,39 @@ export class GeneralInfoComponent implements OnInit {
     this.id = this._Activatedroute.parent.snapshot.paramMap.get("id");
     this.apiService.getGeneralInfoData(this.id).then((res: any) => {
       this.portApiService.getfilterlist().then(filterres => {
-        console.log('Filter Criteria:', filterres)
-        this.filterCriteria = filterres
-        console.log("General Info: ", res)
-        this.generalInfoData = res
-        this.generalInfoForm.patchValue({
-          problemTitle: res.projectData.problemTitle,
-          problemType: res.projectData.problemType,
-          topsGroup: res.topsData ? res.topsData.topsgroup : '',
-          recordCreationDate: res.projectData.createdDate,
-          projectsingle: res.parentProject ? res.parentProject.problemTitle : '',
-          projectsingleid: res.parentProject ? res.parentProject.problemUniqueId : '',
-          submittedBy: res.projectData.problemOwnerName,
-          projectManager: res.portfolioCenterData.pm,
-          sponsor: res.portfolioCenterData.sponsor,
-          projectDescription: res.projectData.projectDescription,
-          primaryProduct: res.primaryProduct,
-          otherImpactedProducts: res.otherImpactedProducts,
-          enviornmentalPortfolio: res.enviornmentalPortfolio
+        this.authService.lookupMaster().then((lookup: any) => {
+          console.log('LookUp Data', lookup)
+          this.lookUpData = lookup
+          console.log('Filter Criteria:', filterres)
+          this.filterCriteria = filterres
+          console.log("General Info:", res)
+          this.generalInfoData = res
+          this.generalInfoForm.patchValue({
+            problemTitle: res.projectData.problemTitle,
+            problemType: res.projectData.problemType,
+            topsGroup: res.topsData ? res.topsData.topsgroup : '',
+            recordCreationDate: res.projectData.createdDate,
+            projectsingle: res.parentProject ? res.parentProject.problemTitle : '',
+            projectsingleid: res.parentProject ? res.parentProject.problemUniqueId : '',
+            submittedBy: res.projectData.problemOwnerName,
+            projectManager: res.portfolioCenterData.pm,
+            sponsor: res.portfolioCenterData.sponsor,
+            projectDescription: res.projectData.projectDescription,
+            primaryProduct: res.primaryProduct,
+            otherImpactedProducts: res.otherImpactedProducts,
+            portfolioOwner: res.portfolioOwner,
+            excecutionScope: res.excecutionScope,
+            enviornmentalPortfolio: res.enviornmentalPortfolio,
+            isOeproject: res.projectData.isOeproject,
+            oeprojectType: res.projectData.oeprojectType == '' ? {} : lookup.find(x => x.lookUpId == res.projectData.oeprojectType),
+            isCapsProject: res.projectData.isCapsProject,
+            isTechTransfer: res.projectData.isTechTransfer,
+            productionStepId: res.projectData.productionStepId,
+            campaignPhaseId: res.projectData.campaignPhaseId,
+            campaignTypeId: res.projectData.campaignTypeId,
+            isQualityRef: false,
+          })
+          this.viewContent = true
         })
       })
     })
@@ -93,7 +119,18 @@ export class GeneralInfoComponent implements OnInit {
   getEnviornmentPortfolio(): any {
     return this.filterCriteria.portfolioOwner.filter(x => x.isEmissionPortfolio == true)
   }
-
+  getoeprojectType(): any {
+    return this.lookUpData.filter(x => x.lookUpParentId == "04D143E7-CAA7-4D8D-88C3-A6CB575890A3")
+  }
+  getCampaignPhase(): any {
+    return this.lookUpData.filter(x => x.lookUpParentId == "183dc1f1-06ba-4022-bd6f-ae07f70751e2")
+  }
+  getCampaignType(): any {
+    return this.lookUpData.filter(x => x.lookUpParentId == "01a49f16-0744-4100-ae8a-ec2e469dbf74")
+  }
+  getProductionStep(): any {
+    return this.lookUpData.filter(x => x.lookUpParentId == "b137412d-8008-4446-8fe6-c56a06b83174")
+  }
 
   submitGeneralInfo() {
 
