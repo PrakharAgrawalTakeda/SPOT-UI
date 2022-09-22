b1816c95ec5bbe17a4a6aa9fecc524648e02110c
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2022-09-22 14:23:09 -0400

    MH 3150 start

--- src/app/modules/project-hub/project-benefits/project-benefits.component.ts
+++ src/app/modules/project-hub/project-benefits/project-benefits.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
+import { AuthService } from 'app/core/auth/auth.service';
+import { ProjectApiService } from '../common/project-api.service';
+import { ProjectHubService } from '../project-hub.service';
 
 @Component({
   selector: 'app-project-benefits',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-benefits.component.scss']
 })
 export class ProjectBenefitsComponent implements OnInit {
-
-  constructor() { }
+  id: string = ''
+  kpiMaster= []
+  lookUpMaster = []
+  projectViewDetails = {}
+  viewContent: boolean = false
+  constructor(public apiService: ProjectApiService, public projecthubservice: ProjectHubService, public auth: AuthService,
+    public fuseAlert: FuseConfirmationService, private _Activatedroute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.dataloader()
   }
-
+  dataloader() {
+    this.id = this._Activatedroute.parent.snapshot.paramMap.get("id");
+    this.apiService.getprojectviewdata(this.id).then((res: any) => {
+      this.auth.KPIMaster().then((kpis:any) => {
+        this.auth.lookupMaster().then((lookup:any) => {
+          this.projectViewDetails = res
+          this.kpiMaster = kpis
+          this.lookUpMaster = lookup
+          this.viewContent = true
+        })
+      })
+    })
+  }
 }
