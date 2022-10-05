ee812570b147c9a5c866b621e9e66074672ad5ad
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2022-10-05 18:46:32 -0400

    MH 2889 progress

--- src/app/modules/project-hub/general-info/general-info-single-edit/general-info-single-edit.component.ts
+++ src/app/modules/project-hub/general-info/general-info-single-edit/general-info-single-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
+import { ProjectApiService } from '../../common/project-api.service';
+import { ProjectHubService } from '../../project-hub.service';
 
 @Component({
   selector: 'app-general-info-single-edit',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-info-single-edit.component.scss']
 })
 export class GeneralInfoSingleEditComponent implements OnInit {
-
-  constructor() { }
+  filterCriteria: any =  {}
+  generalInfo = {}
+  viewContent = false
+  generalInfoForm = new FormGroup({
+    problemTitle: new FormControl(''),
+    parentProgram: new FormControl(''),
+    problemType: new FormControl('Standard Project / Program'),
+    projectDescription: new FormControl(''),
+    primaryProduct: new FormControl({}),
+    otherImpactedProducts: new FormControl([]),
+    portfolioOwner: new FormControl({}),
+    excecutionScope: new FormControl([]),
+    enviornmentalPortfolio: new FormControl({}),
+    isArchived: new FormControl(false)
+  })
+  constructor(private apiService: ProjectApiService,
+    public projectHubService: ProjectHubService,
+    public fuseAlert: FuseConfirmationService) { }
 
   ngOnInit(): void {
+    this.apiService.getGeneralInfoData(this.projectHubService.projectid).then((res: any) => {
+      this.generalInfo = res
+      this.filterCriteria = this.projectHubService.all
+      this.generalInfoForm.patchValue({
+        problemTitle: res.projectData.problemTitle,
+        problemType: res.projectData.problemType,
+        parentProgram: res.parentProject ? res.parentProject.problemTitle : '',
+        projectDescription: res.projectData.projectDescription,
+        primaryProduct: res.primaryProduct ? res.primaryProduct.fullProductName : '',
+        otherImpactedProducts: res.otherImpactedProducts ? res.otherImpactedProducts : [],
+        portfolioOwner: res.portfolioOwner ? res.portfolioOwner.portfolioOwner : '',
+        excecutionScope: res.excecutionScope ? res.excecutionScope : [],
+        enviornmentalPortfolio: res.enviornmentalPortfolio ? res.enviornmentalPortfolio.portfolioOwner : '',
+        isArchived: res.projectData.isArchived
+      })
+      this.viewContent = true
+    })
   }
 
+
+  getExcecutionScope(): any {
+    return this.filterCriteria.portfolioOwner.filter(x => x.isExecutionScope == true)
+  }
+
+
+  submitGI(){
+
+  }
 }
