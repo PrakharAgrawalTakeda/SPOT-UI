c071ac5a9ca52ae13cc8a7d93c4e725577a9bdaa
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2022-10-12 08:44:05 -0400

    bug fix

--- src/app/shared/global-variables.ts
+++ src/app/shared/global-variables.ts
@@ -5,9 +5,9 @@ export class GlobalVariables {
     public static spotui: string =  process.env.NODE_ENV === 'development' ? 'http://localhost:4200/' : 'https://salmon-grass-03d30f90f.1.azurestaticapps.net/'
 
     //SPOT API APPLICATION VARIABLES
-    // public static apiurl: string = 'https://spot4api-dev.azurewebsites.net/api/'
+     public static apiurl: string = 'https://spot4api-dev.azurewebsites.net/api/'
     // Prakhar Local API
     //public static apiurl: string = 'https://localhost:5001/api/'
     //public static apiurl: string = 'https://localhost:44304/api/'
-    public static apiurl: string = 'https://localhost:7288/api/'
+    //public static apiurl: string = 'https://localhost:7288/api/'
 }
