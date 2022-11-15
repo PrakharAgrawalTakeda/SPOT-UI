ece73faadfef06ad8e80b213ddfaad346b881aa9
Mannat Dev <mannat.dev@takeda.com>
2022-11-15 14:58:30 -0500

    baseline comparison

--- src/app/modules/project-hub/project-board/link-milestones/link-milestones.component.ts
+++ src/app/modules/project-hub/project-board/link-milestones/link-milestones.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProjectHubService } from '../../project-hub.service';
 
 @Component({
   selector: 'app-link-milestones',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link-milestones.component.scss']
 })
 export class LinkMilestonesComponent implements OnInit {
-
-  constructor() { }
+  toggleHelper: boolean = false
+  localIncludedItems = new FormGroup({
+    toggle: new FormControl(false)
+  })
+  constructor(public projecthubservice: ProjectHubService) { }
 
   ngOnInit(): void {
+  }
+
+  toggleSchedule(event: any) {
+    this.toggleHelper = true
+    this.projecthubservice.includeClosedItems.schedule.next(event.checked)
+  }
+
+  submitlink() {
+    
   }
 
 }
