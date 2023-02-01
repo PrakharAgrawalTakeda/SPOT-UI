ef7c7dc57c8f5288aca1096f36b88c0d2cd279c5
prakharagrawal <prakhar.agrawal@takeda.com>
2023-02-01 10:50:34 -0500

    MH 3862 almost final

---
+++ src/app/modules/project-hub/common/option-info/option-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OptionInfoComponent } from './option-info.component';
+
+describe('OptionInfoComponent', () => {
+  let component: OptionInfoComponent;
+  let fixture: ComponentFixture<OptionInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OptionInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OptionInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
