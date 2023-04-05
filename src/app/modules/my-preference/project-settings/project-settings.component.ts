a9303539adc7446cea9039af8eea99e835078301
Mannat Dev <mannat.dev@takeda.com>
2023-04-05 17:14:09 -0400

    archived toggle

--- src/app/modules/my-preference/project-settings/project-settings.component.ts
+++ src/app/modules/my-preference/project-settings/project-settings.component.ts
@@ -20,19 +20,31 @@ export class ProjectSettingsComponent {
     includeArchiveProject: new FormControl(false)
   })
   lookupdata: any = []
-  constructor(private titleService: Title, public auth: AuthService, private roleService: RoleService, private apiService: MyPreferenceApiService, private msalService: MsalService) { }
+  private initialized = false;
+
+  constructor(private titleService: Title, public auth: AuthService, private roleService: RoleService, private apiService: MyPreferenceApiService, private msalService: MsalService) {
+    this.archiveForm.valueChanges.subscribe(res => {
+      console.log(this.archiveForm.getRawValue())
+      if (this.initialized) {
+        this.changeToggle();
+      } else {
+        this.initialized = true;
+      }
+
+    })
+  }
 
   ngOnInit(): void {
     console.log("Project settings")
-    this.auth.lookupMaster().then(res=>{
-      this.lookupdata =  res
+    this.auth.lookupMaster().then(res => {
+      this.lookupdata = res
       this.preferenceForm.patchValue({
         role: this.roleService.roleMaster.securityGroupId
       })
       this.titleService.setTitle("My Preferences")
     })
     this.dataloader()
-    
+
   }
 
   dataloader() {
@@ -45,22 +57,31 @@ export class ProjectSettingsComponent {
     })
   }
 
-  getRoles(): any{
-    return this.lookupdata.filter(x => x.lookUpParentId == '3FF934A4-D5FC-4F92-AE75-78A5EBC64A1B' &&   !['C005FB71-C1FF-44D3-8779-5CA37643D794','BDC4DF5A-14D6-4468-9238-B933CA6C1B46','500ee862-3878-43d9-9378-53feb1832cef'].includes(x.lookUpId)  ).sort((a, b) => {
+  getRoles(): any {
+    return this.lookupdata.filter(x => x.lookUpParentId == '3FF934A4-D5FC-4F92-AE75-78A5EBC64A1B' && !['C005FB71-C1FF-44D3-8779-5CA37643D794', 'BDC4DF5A-14D6-4468-9238-B933CA6C1B46', '500ee862-3878-43d9-9378-53feb1832cef'].includes(x.lookUpId)).sort((a, b) => {
       return a.lookUpOrder - b.lookUpOrder;
     })
   }
 
-  updateRole(value: any){
+  updateRole(value: any) {
     console.log(this.msalService.instance.getActiveAccount().localAccountId)
     console.log(value.value)
-    this.apiService.updateRole(this.msalService.instance.getActiveAccount().localAccountId,value.value).then(res=>{
+    this.apiService.updateRole(this.msalService.instance.getActiveAccount().localAccountId, value.value).then(res => {
       location.reload()
     })
   }
 
-  updateToggleValue(value: any) {
-
+  changeToggle() {
+    this.initialized = false
+    var archiveProject = this.archiveForm.getRawValue();
+    var mainObj = {
+      userAdid: archiveProject.userAdid,
+      includeArchiveProject: archiveProject.includeArchiveProject
+    }
+    console.log(mainObj)
+    this.apiService.updateuserPreference(this.msalService.instance.getActiveAccount().localAccountId, mainObj).then(res => {
+      location.reload()
+    })
   }
 
 }
