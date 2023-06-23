58d2dcd5d02bbe47ea9c02a8530c82bef34f4313
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2023-06-23 15:25:49 -0400

    New project sorting style, now sortable by only confidential projects

--- src/app/modules/project-hub/common/project-single-dropdown/project-single-dropdown.component.ts
+++ src/app/modules/project-hub/common/project-single-dropdown/project-single-dropdown.component.ts
@@ -2,8 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { GlobalVariables } from 'app/shared/global-variables';
-import {debounceTime, filter, lastValueFrom, map, Observable, startWith, Subject, takeUntil} from 'rxjs';
-import {ProjectHubService} from "../../project-hub.service";
+import { debounceTime, filter, lastValueFrom, map, Observable, startWith, Subject, takeUntil } from 'rxjs';
+import { ProjectHubService } from "../../project-hub.service";
 import { RoleService } from 'app/core/auth/role.service';
 
 @Component({
@@ -18,7 +18,7 @@ export class ProjectSingleDropdownComponent implements OnInit {
   @Input() showHint: boolean = false
   @Input() hint: string = ''
   @Input() hideRelations: boolean = false
-  @Input() confiedentialProjects: 'None' | 'User' = 'User'
+  @Input() confidentialProjects: 'None' | 'User' | 'Only' = 'User'
 
   options: string[] = ['One', 'Two', 'Three'];
   resultSets: any[];
@@ -26,7 +26,7 @@ export class ProjectSingleDropdownComponent implements OnInit {
   debounce = 400
   filteredOptions: any
   private _unsubscribeAll: Subject<any> = new Subject<any>();
-  constructor(private _httpClient: HttpClient,public projecthubservice: ProjectHubService,private roleService: RoleService) {
+  constructor(private _httpClient: HttpClient, public projecthubservice: ProjectHubService, private roleService: RoleService) {
   }
 
   ngOnInit(): void {
@@ -40,7 +40,7 @@ export class ProjectSingleDropdownComponent implements OnInit {
         // so the autocomplete panel can be closed
         if (!value || value.length < this.minLength) {
           this.resultSets = null;
-          if (value == ''){
+          if (value == '') {
             this.formgroup.patchValue({
               projectsingle: '',
               projectsingleid: ''
@@ -59,37 +59,43 @@ export class ProjectSingleDropdownComponent implements OnInit {
         const params = new HttpParams().set('query', value);
         this._httpClient.post(GlobalVariables.apiurl + `Projects/Search?${params.toString()}`, { body: [] })
           .subscribe((resultSets: any) => {
-              let abc$;
-              if (this.hideRelations) {
-                  var ids = [];
-                  abc$ = this._httpClient.get(GlobalVariables.apiurl + `ProjectHubData/ProjectTree/${this.projecthubservice.projectid}`)
-                  const response =  lastValueFrom(abc$)
-                  response.then((res: any) => {
-                      res.values.forEach(project => {
-                          ids.push(project.problemUniqueId);
-                      })
-                      this.projecthubservice.removedIds = ids;
-                      for (var i = 0; i < resultSets.projectData.length; i++) {
-                          var obj = resultSets.projectData[i];
-                          if (this.projecthubservice.removedIds.indexOf(obj.problemUniqueId) !== -1) {
-                              resultSets.projectData.splice(i, 1);
-                              i--;
-                          }
-                      }
-                  })
-
-
-              }
-            // Store the result sets
-            this.resultSets = resultSets.projectData?.filter(x => !x.isConfidential);
-            if (this.roleService.roleMaster?.confidentialProjects && this.confiedentialProjects == 'User') {
-                if (this.roleService.roleMaster.confidentialProjects.length > 0) {
-                    var confProjectUserList = resultSets.projectData?.filter(x=>this.roleService.roleMaster.confidentialProjects.includes(x.problemUniqueId) )
-                    if(confProjectUserList?.length>0){
-                        console.log(confProjectUserList)
-                        this.resultSets = [...this.resultSets, ...confProjectUserList]
-                    }
+            let abc$;
+            if (this.hideRelations) {
+              var ids = [];
+              abc$ = this._httpClient.get(GlobalVariables.apiurl + `ProjectHubData/ProjectTree/${this.projecthubservice.projectid}`)
+              const response = lastValueFrom(abc$)
+              response.then((res: any) => {
+                res.values.forEach(project => {
+                  ids.push(project.problemUniqueId);
+                })
+                this.projecthubservice.removedIds = ids;
+                for (var i = 0; i < resultSets.projectData.length; i++) {
+                  var obj = resultSets.projectData[i];
+                  if (this.projecthubservice.removedIds.indexOf(obj.problemUniqueId) !== -1) {
+                    resultSets.projectData.splice(i, 1);
+                    i--;
+                  }
                 }
+              })
+
+
+            }
+            // Store the result sets
+            if (this.confidentialProjects != 'Only') 
+            {
+              this.resultSets = resultSets.projectData?.filter(x => !x.isConfidential);
+            }
+            else{
+              this.resultSets = []
+            }
+            if (this.roleService.roleMaster?.confidentialProjects && this.confidentialProjects != 'None') {
+              if (this.roleService.roleMaster.confidentialProjects.length > 0) {
+                var confProjectUserList = resultSets.projectData?.filter(x => this.roleService.roleMaster.confidentialProjects.includes(x.problemUniqueId))
+                if (confProjectUserList?.length > 0) {
+                  console.log(confProjectUserList)
+                  this.resultSets = [...this.resultSets, ...confProjectUserList]
+                }
+              }
             }
             console.log(this.resultSets)
             console.log(GlobalVariables.apiurl + `Projects/Search?${params.toString()}`)
