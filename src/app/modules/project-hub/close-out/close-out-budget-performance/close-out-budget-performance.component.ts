33613f4881853df3e1944c59e23a59dba1af2a54
Toader Morosan <toader.morosan@softwareOne.com>
2023-07-12 11:23:18 +0200

    Budget fixes

--- src/app/modules/project-hub/close-out/close-out-budget-performance/close-out-budget-performance.component.ts
+++ src/app/modules/project-hub/close-out/close-out-budget-performance/close-out-budget-performance.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {ProjectHubService} from "../../project-hub.service";
 import {ActivatedRoute} from "@angular/router";
