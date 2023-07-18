8d07ffe65bca90ba5fb55ad91768adc1fc8f8176
prakharagrawal <prakhar.agrawal@takeda.com>
2023-07-18 15:50:57 -0400

    0004789: Implement Changes related to Value Creation

---
+++ src/app/modules/project-hub/close-out/close-out-value-creation/close-out-value-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close-out-value-creation',
+  templateUrl: './close-out-value-creation.component.html',
+  styleUrls: ['./close-out-value-creation.component.scss']
+})
+export class CloseOutValueCreationComponent {
+
+}
