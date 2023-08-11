8bf8485d273fca049cffc8c9e5beeb42650b3d40
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2023-08-11 16:06:01 -0400

    2877: Project Documents page - Develop Create a library

--- src/app/modules/project-hub/common/project-api.service.ts
+++ src/app/modules/project-hub/common/project-api.service.ts
@@ -859,5 +859,12 @@ async updateBusinessCaseFunding(body, projectId, optionid){
     const response = await lastValueFrom(abc$)
     return response
   }
+  async addProjectDocumentLibrary(body,id) {
+    var link = GlobalVariables.apiurl + "ProjectDocuments/CreateLibrary/"+id
+    const abc$ = this.http.post(link, body)
+    const response = await lastValueFrom(abc$)
+    return response
+  }
+
   }
 
