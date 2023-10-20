208c795a668f6b6957350a3356c473b58210366c
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2023-10-20 16:57:04 -0400

    0003282: Adjust myPreferences Role-Assignment page: enable this feature only for users with Business Admin Role -- Last Phase

--- src/app/modules/admin/manage-users-edit/manage-users-edit.component.ts
+++ src/app/modules/admin/manage-users-edit/manage-users-edit.component.ts
@@ -1,13 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { primaryRoles, secondaryRoles } from 'app/shared/roles';
+import { RoleService } from 'app/core/auth/role.service';
 @Component({
   selector: 'app-manage-users-edit',
   templateUrl: './manage-users-edit.component.html',
   styleUrls: ['./manage-users-edit.component.scss']
 })
 export class ManageUsersEditComponent implements OnInit {
-  viewContent= true
-  constructor( public adminService: AdminService) {
+  viewContent = true
+  managerUserRolesForm = new FormGroup({
+    user: new FormControl({}),
+    userPrimaryRole: new FormControl({}),
+    userSecondaryRoles: new FormControl([])
+  })
+  primaryRoles = primaryRoles
+  secondaryRoles = secondaryRoles
+  isUserSelected: boolean = false
+  constructor(public adminService: AdminService, private roleService: RoleService) {
+    this.managerUserRolesForm.controls.user.valueChanges.subscribe((res: any) => {
+      console.log(res)
+      if (Object.keys(res).length > 0) {
+        this.roleService.getCurrentRole(res.userAdid).then((resp: any) => {
+          console.log(resp)
+          this.managerUserRolesForm.patchValue({
+            userPrimaryRole: resp.securityGroupId
+          })
+          this.isUserSelected = true
+        })
+      }
+      else {
+        this.isUserSelected = false
+        this.managerUserRolesForm.patchValue({
+          userPrimaryRole: {},
+          userSecondaryRoles: []
+        })
+      }
+      console.log(this.managerUserRolesForm.getRawValue())
+    })
   }
   ngOnInit(): void {
   }
