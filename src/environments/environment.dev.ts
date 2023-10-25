bd370cacc69a8d53222a16b7854acc4cf30fd42b
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2023-10-25 17:25:32 -0400

    Update SPOT API URLs for Development Enviornments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     environment: 'DEV',
     SPOT_UI_URL: 'https://lively-sky-0c3c7e80f.2.azurestaticapps.net/',
-    SPOT_API_URL:'https://spotapi-gateway-dev.azurewebsites.net/api/'
+    SPOT_API_URL:'https://tospotdev-api-gateway.azurewebsites.net/api/'
 };
