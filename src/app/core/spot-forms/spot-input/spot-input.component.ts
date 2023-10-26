e6821fe271b063029cf2ae2d7a69397197f2010f
Toader Morosan <toader.morosan@softwareOne.com>
2023-10-26 15:04:31 +0200

    Spot input: added strong label option

--- src/app/core/spot-forms/spot-input/spot-input.component.ts
+++ src/app/core/spot-forms/spot-input/spot-input.component.ts
@@ -25,6 +25,7 @@ export class SpotInputComponent implements OnInit, ControlValueAccessor {
   @Input() Required: boolean = false;
   @Input() callLocation: 'View' | 'Edit' = 'Edit'
   @Input() allowNegativeValues: boolean = false
+  @Input() isLabelStrong: boolean = false
 
 
   formFieldHelpers: any
@@ -64,54 +65,54 @@ export class SpotInputComponent implements OnInit, ControlValueAccessor {
         value = value.replace(/\..*/, '');
       }
       const formattedValue = value?.replace(/(?<!\.\d*)(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-  
+
       this.control.setValue(formattedValue);
     }
     else {
       this.control.setValue(val);
     }
   }
-  
+
   setDisabledState(isDisabled: boolean) {
     isDisabled == true ? this.control.disable() : this.control.enable()
   }
-  
+
   formatInput(event: any): void {
     const isFocused = document.activeElement === event.target;
     if (this.inputType === 'Number') {
       let value = event.target.value;
-  
+
       // Remove non-numeric, non-decimal, and non-negative sign characters
       const regex = this.allowNegativeValues ? /[^\d.-]/g : /[^\d.]/g;
       value = value.replace(regex, '');
-  
+
       if (this.allowNegativeValues) {
         // Allow only one negative sign and ensure it is at the beginning
         value = value.replace(/(?!^)-/g, '');
       }
-  
+
       if (this.decimalCount === 0) {
         // Remove any decimal points if decimalCount is 0
         value = value.replace(/\..*/, '');
       } else {
         // Allow only one decimal point
         value = value.replace(/(\..*)\./g, '$1');
-  
+
         // Round the decimal value to decimalCount decimal places if needed
         const decimalIndex = value.indexOf('.');
         if (decimalIndex !== -1 && decimalIndex + this.decimalCount + 1 < value.length) {
           value = parseFloat(value)?.toFixed(this.decimalCount);
         }
       }
-  
+
       // Add commas as thousand separators only when the field is blurred
       if (!isFocused) {
         value = value.replace(/(?<!\.\d*)(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
       }
-  
+
       // Update the input field value
       event.target.value = value;
-  
+
       // Call the onChange method with the float value
       this.onChange(parseFloat(value));
     } else {
@@ -121,26 +122,26 @@ export class SpotInputComponent implements OnInit, ControlValueAccessor {
 
   onBlur(event: any): void {
     this.onTouch();
-  
+
     if (this.autoAddDecimal && this.decimalCount > 0 && event?.target?.value) {
       let value = event.target.value;
-  
+
       // Remove commas from the value
       const valueWithoutCommas = value.replace(/,/g, '');
-  
+
       // Check if the value has a decimal point
       if (valueWithoutCommas.indexOf('.') === -1) {
         // Add the decimal point and the required number of decimal places
         value = parseFloat(valueWithoutCommas)?.toFixed(this.decimalCount);
-  
+
         // Add commas as thousand separators
         value = value.replace(/(?<!\.\d*)(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-  
+
         // Update the input field value
         event.target.value = value;
       }
     }
-  
+
     // Add commas as thousand separators when the field is blurred
     if (event?.target?.value) {
       const value = event.target.value.replace(/(?<!\.\d*)(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
