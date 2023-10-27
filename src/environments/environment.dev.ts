e67a01c8488523a9be48ada7ca9e7fb8de10c264
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2023-10-27 07:32:49 -0400

    final decomission of LivelySky

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     environment: 'DEV',
-    SPOT_UI_URL: 'https://lively-sky-0c3c7e80f.2.azurestaticapps.net/',
+    SPOT_UI_URL: 'https://tospotdev.azurewebsites.net/',
     SPOT_API_URL:'https://tospotdev-api-gateway.azurewebsites.net/api/'
 };
