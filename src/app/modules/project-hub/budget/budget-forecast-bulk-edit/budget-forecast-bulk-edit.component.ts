05e6573a6aaae8615b7fa7c43573d0ca53e3f7ca
Toader Morosan <toader.morosan@softwareOne.com>
2024-01-20 17:34:19 +0100

    hotfix budget

--- src/app/modules/project-hub/budget/budget-forecast-bulk-edit/budget-forecast-bulk-edit.component.ts
+++ src/app/modules/project-hub/budget/budget-forecast-bulk-edit/budget-forecast-bulk-edit.component.ts
@@ -738,7 +738,7 @@ export class BudgetForecastBulkEditComponent {
         this.recalculateMtdp()
         this.budgetForecastForm.patchValue({
             totalApprovedCapex: budget.totalApprovedCapEx ? budget.totalApprovedCapEx : 0,
-            committedSpend: forecast.find(x => x.active == 'Current').committedSpend,
+            committedSpend: forecast.find(x => x.isopen && x.budgetData== "CapEx Forecast").committedSpend,
             afpDeviationCode: this.projecthubservice.lookUpMaster.find(x => x.lookUpId == forecast.find(x => x.active == 'Current').afpDeviationCodeID),
             mtdpDeviationCode: this.projecthubservice.lookUpMaster.find(x => x.lookUpId == forecast.find(x => x.active == 'Current').mtdpDeviationCodeID),
         })
