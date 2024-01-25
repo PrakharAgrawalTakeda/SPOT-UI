e63b422530d40b38d3537617231d9a7a67e851dc
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2024-01-25 16:29:30 -0500

    Testing MH 5380 Again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,28 @@ import { environment } from 'environments/environment';
 import { AppConfig } from './core/config/app.config';
 
 @Component({
-    selector   : 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss']
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent
-{
+export class AppComponent {
     /**
      * Constructor
      */
     config: AppConfig;
-    constructor(private authService: MsalService, private appService: AppService,private snack: MatSnackBar, private _fuseConfigService: FuseConfigService)
-    {
+    constructor(private authService: MsalService, private appService: AppService, private snack: MatSnackBar, private _fuseConfigService: FuseConfigService) {
         this._fuseConfigService.config$
             .subscribe((config: AppConfig) => {
-                var theme: any = config.themes.find(x=>x.id == 'theme-teal')?.id
-                this._fuseConfigService.config = {theme}
+                if (environment.environment == 'Local') {
+                    var theme: any = config.themes.find(x => x.id == 'theme-teal')?.id
+                    this._fuseConfigService.config = { theme }
+                }
                 // Store the config
                 this.config = config;
             });
-    
-            
-        
+
+
+
         this.appService.errorSave.subscribe(res => {
             if (res == true) {
                 this.snack.open("An Error has Occured, Please contact the SPOT Support Team by sending an e-mail to DL.SPOTSupport@takeda.com", "Dismiss", {
