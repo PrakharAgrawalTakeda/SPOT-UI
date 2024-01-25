f8bb5abbd28301bc5e0c492327ed4c6c019f579e
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2024-01-25 16:27:02 -0500

    Testing MH 5380

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 import { AppService } from './app.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FuseConfigService } from '@fuse/services/config';
+import { environment } from 'environments/environment';
+import { AppConfig } from './core/config/app.config';
 
 @Component({
     selector   : 'app-root',
@@ -13,8 +16,19 @@ export class AppComponent
     /**
      * Constructor
      */
-    constructor(private authService: MsalService, private appService: AppService,private snack: MatSnackBar)
+    config: AppConfig;
+    constructor(private authService: MsalService, private appService: AppService,private snack: MatSnackBar, private _fuseConfigService: FuseConfigService)
     {
+        this._fuseConfigService.config$
+            .subscribe((config: AppConfig) => {
+                var theme: any = config.themes.find(x=>x.id == 'theme-teal')?.id
+                this._fuseConfigService.config = {theme}
+                // Store the config
+                this.config = config;
+            });
+    
+            
+        
         this.appService.errorSave.subscribe(res => {
             if (res == true) {
                 this.snack.open("An Error has Occured, Please contact the SPOT Support Team by sending an e-mail to DL.SPOTSupport@takeda.com", "Dismiss", {
