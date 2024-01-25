3e55babb84820f9a82bc9d827442a6443d78156f
Prakhar Vikas Agrawal <prakhar.agrawal@takeda.com>
2024-01-25 17:12:46 -0500

    Implement a Deployment Screen

--- src/app/modules/landing/home/home.routing.ts
+++ src/app/modules/landing/home/home.routing.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { LandingHomeComponent } from 'app/modules/landing/home/home.component';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { UnderDeploymentComponent } from './under-deployment/under-deployment.component';
 
 export const landingHomeRoutes: Route[] = [
     {
@@ -10,6 +11,10 @@ export const landingHomeRoutes: Route[] = [
     {
         path     : 'coming-soon',
         component: ComingSoonComponent
+    },
+    {
+        path     : 'under-deployment',
+        component: UnderDeploymentComponent
     }
 
 ];
