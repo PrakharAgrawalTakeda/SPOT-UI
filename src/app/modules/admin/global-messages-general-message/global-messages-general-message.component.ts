4638a7796db634c222967b2b5443b331bf5f7a86
PrakharAgrawalTakeda <prakhar.agrawal@takeda.com>
2024-03-29 14:41:18 -0400

    "Add table view for global messages in admin module and load data from API"
    This commit adds a new table view in the `global-messages-general-message` component of the admin module, which displays a list of global messages with columns for Message Id, Message Title, Message Creation Date, and Active. The table also includes action buttons for expanding the row, editing, and deleting a single message. The data for the table is loaded from an API call using the `AdminApiService`. The `toggleExpandRow` function is also added to toggle the expansion of a row. The `viewContent` property is used to conditionally render the table based on the data availability.

--- src/app/modules/admin/global-messages-general-message/global-messages-general-message.component.ts
+++ src/app/modules/admin/global-messages-general-message/global-messages-general-message.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AdminService } from '../admin.service';
+import { AdminApiService } from '../admin-api.service';
 
 @Component({
   selector: 'app-global-messages-general-message',
   templateUrl: './global-messages-general-message.component.html',
-  styleUrls: ['./global-messages-general-message.component.scss']
+  styleUrls: ['./global-messages-general-message.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class GlobalMessagesGeneralMessageComponent {
+export class GlobalMessagesGeneralMessageComponent implements OnInit {
+  globalMessages: any = []
+  @ViewChild('GlobalMessageTable') table: any;
+  viewContent: boolean = false;
+    constructor(public adminService: AdminService, private adminApiService: AdminApiService) { }
+  
+    ngOnInit(): void {
+     this.dataloader();
+    }
 
+    dataloader(){
+      this.adminApiService.getGlobalMessages().then((res: any) => {
+        console.log(res)
+        this.globalMessages = res
+        this.viewContent = true;
+      })
+
+    }
+    toggleExpandRow(row) {
+      this.table.rowDetail.toggleExpandRow(row);
+    }
 }
