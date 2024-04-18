22b21bd10f29591bd27dfe56f9ed1159579a6d6e
PrakharAgrawalTakeda <prakhar.agrawal@takeda.com>
2024-04-18 10:07:06 -0400

    Rolling back API Gateway

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     environment: 'DEV',
     SPOT_UI_URL: 'https://tospotdev.azurewebsites.net/',
-    SPOT_API_URL:'https://tospotdev-api.azurewebsites.net/api/',
+    SPOT_API_URL:'https://tospotdev-api-gateway.azurewebsites.net/api/',
     isDeploying: false
 };
