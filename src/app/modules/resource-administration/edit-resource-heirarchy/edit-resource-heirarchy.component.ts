58512b0a17d4f149dd758a8bfae053880630cad1
Mannat Dev <mannat.dev@takeda.com>
2024-05-12 20:58:11 -0400

    RM & Schedule bulk edit Resp width

--- src/app/modules/resource-administration/edit-resource-heirarchy/edit-resource-heirarchy.component.ts
+++ src/app/modules/resource-administration/edit-resource-heirarchy/edit-resource-heirarchy.component.ts
@@ -4,6 +4,8 @@ import { MsalService } from '@azure/msal-angular';
 import { FuseNavigationService } from '@fuse/components/navigation';
 import { AuthService } from 'app/core/auth/auth.service';
 import { ResourceAdministrationService } from '../resource-administration.service';
+import { ProjectApiService } from 'app/modules/project-hub/common/project-api.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-resource-heirarchy',
@@ -12,12 +14,57 @@ import { ResourceAdministrationService } from '../resource-administration.servic
 })
 export class EditResourceHeirarchyComponent implements OnInit {
   viewContent: boolean = false
-  constructor(private router: Router, private _fuseNavigationService: FuseNavigationService, public auth: AuthService, private authService: MsalService,
-    public resourceadminservice: ResourceAdministrationService) {
+  RMForm = new FormGroup({
+    portfolioOwner: new FormControl(null)
+  })
+  portfolioOwner: any;
+  userManagedPortfolios: any;
+  constructor(private router: Router, private _fuseNavigationService: FuseNavigationService, public auth: AuthService,
+    public resourceadminservice: ResourceAdministrationService, private apiService: ProjectApiService, private msalService: MsalService) {
 
   }
   ngOnInit(): void {
     console.log("I'm here")
+    const currentUserID = this.msalService.instance.getActiveAccount().localAccountId;
+
+    this.apiService.getfilterlist().then(res => {
+      this.auth.lookupMaster().then(lookupMaster => {
+      console.log(lookupMaster)
+
+      this.portfolioOwner = res
+      const portfolioOwnerList = this.portfolioOwner.portfolioOwner;
+            this.userManagedPortfolios = portfolioOwnerList.filter(po => {
+            // Check if currentUserID matches resourceMgrAdId
+            return po.resourceMgrAdId == currentUserID;
+        }).map(po => po.portfolioOwner);
+            console.log(this.userManagedPortfolios)
+      })
+    })
     this.viewContent = true;
+
   }
+
+  // getPortfolioOwner(): any {
+  //   return this.portfolioOwner.portfolioOwner.filter(x => x.isPortfolioOwner == true)
+  //   // return "";
+  // }
+  // fetchPortfolioFunctions() {
+  //   // Assuming you have a service that fetches the state of each function for the selected portfolio
+  //   this.portfolioService.getFunctionsForPortfolio(this.selectedPortfolio).subscribe((data: Function[]) => {
+  //     this.functions = this.functions.map(f => ({
+  //       ...f,
+  //       selected: !!data.find(df => df.id === f.id)
+  //     }));
+  //   });
+  // }
+  
+  // toggleFunction(functionId: number) {
+  //   // Add or remove the function from the selected portfolio
+  //   if (this.functions.find(f => f.id === functionId).selected) {
+  //     this.portfolioService.addFunctionToPortfolio(this.selectedPortfolio, functionId);
+  //   } else {
+  //     this.portfolioService.removeFunctionFromPortfolio(this.selectedPortfolio, functionId);
+  //   }
+  // }
+  
 }
