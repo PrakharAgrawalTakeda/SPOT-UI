b2a67c30204d83bcf55e80dd06a74fd6d40717f6
PrakharAgrawalTakeda <prakhar.agrawal@takeda.com>
2024-05-22 20:00:51 -0400

    Update isDeploying flag in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     environment: 'PROD',
     SPOT_UI_URL: 'https://tospot.azurewebsites.net/',
     SPOT_API_URL:'https://tospot-api.azurewebsites.net/api/',
-    isDeploying: false
+    isDeploying: true
 };
