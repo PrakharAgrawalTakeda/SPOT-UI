4c0ff4bdca310227ba744662d2e5f89053e90ba2
Prakhar Agrawal <prakhar.agrawal@takeda.com>
2024-05-30 10:38:04 -0400

    Add detailed schedule component for project hub

---
+++ src/app/modules/project-hub/detailed-schedule/detailed-schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-schedule',
+  templateUrl: './detailed-schedule.component.html',
+  styleUrls: ['./detailed-schedule.component.scss']
+})
+export class DetailedScheduleComponent {
+
+}
