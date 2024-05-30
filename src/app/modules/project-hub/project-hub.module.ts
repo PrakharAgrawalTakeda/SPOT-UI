24298bd0c9d66d9f76a55534c673db184794ceb8
Prakhar Agrawal <prakhar.agrawal@takeda.com>
2024-05-30 09:51:16 -0400

    Add Bryntum Gantt module for project scheduling

--- src/app/modules/project-hub/project-hub.module.ts
+++ src/app/modules/project-hub/project-hub.module.ts
@@ -188,6 +188,7 @@ import { NewMetricsComponent } from './project-benefits/new-metrics/new-metrics.
 // import { SpotInputForecastComponent } from 'app/core/spot-forms/spot-input-forecast/spot-input-forecast.component';
 import { EditMetricsComponent } from './project-benefits/edit-metrics/edit-metrics.component';
 import { SpotGlobalMessageModule } from 'app/layout/common/spot-global-message/spot-global-message.module';
+import { BryntumGanttModule } from '@bryntum/gantt-angular';
 import {data} from "autoprefixer";
 
 export const projectRoutes: Route[] = [
@@ -658,6 +659,7 @@ export const projectRoutes: Route[] = [
     ShortcutsModule,
     MessagesModule,
     NotificationsModule,
+    BryntumGanttModule,
     UserModule,
     SpotGlobalMessageModule
     ],
